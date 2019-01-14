787f8fdb7cc15e20ddee9f7b6fe31cd5e27d36f7
Apple <apple@Apples-MacBook-Pro.local>
2019-01-14 23:26:59 +0300

    feat: setup connection

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Action, User, Message, Event } from './shared/model';
+import { SocketService } from './shared/services/socket.service';
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'tcc-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit {
+  action = Action;
+  user: User;
+  messages: Message[] = [];
+  messageContent: string;
+  ioConnection: Subscription;
 
-  constructor() { }
+  constructor(private socketService: SocketService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.initIoConnection();
   }
 
+  private initIoConnection(): void {
+    this.socketService.initSocket();
+
+    this.ioConnection = this.socketService.onMessage().subscribe((message: Message) => {
+      this.messages.push(message);
+    });
+
+    this.socketService.onEvent(Event.CONNECT).subscribe(() => console.log('connected'));
+
+    this.socketService.onEvent(Event.DISCONNECT).subscribe(() => console.log('disconnected'));
+  }
 }
